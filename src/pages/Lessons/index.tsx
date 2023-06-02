9bc1d88d89f3c0dcb990ed157eba52e9fefdd09e
douqiting <douqiting@onlineinline.com>
2023-06-02 14:09:43 +0800

    feat(课程): 课程列表修改

--- src/pages/Lessons/index.tsx
+++ src/pages/Lessons/index.tsx
@@ -1,7 +1,7 @@
 import request from '@/utils/request';
-import type { ProColumns } from '@ant-design/pro-components';
+import type { ActionType, ProColumns } from '@ant-design/pro-components';
 import { PageContainer, ProTable } from '@ant-design/pro-components';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 type RowItem = {
   id: number;
@@ -49,6 +49,42 @@ const columns: ProColumns<RowItem>[] = [
     dataIndex: 'level',
     copyable: true,
     ellipsis: true,
+    valueType: 'select',
+    valueEnum: {
+      all: { text: '超长'.repeat(50) },
+      open: {
+        text: '未解决',
+        status: 'Error',
+      },
+      closed: {
+        text: '已解决',
+        status: 'Success',
+        disabled: true,
+      },
+      processing: {
+        text: '解决中',
+        status: 'Processing',
+      },
+    },
+  },
+  {
+    title: '操作',
+    valueType: 'option',
+    key: 'option',
+    render: (text, record, _, action) => [
+      <a key="editable">编辑</a>,
+      <a href={record.url} target="_blank" rel="noopener noreferrer" key="view">
+        查看
+      </a>,
+      // <TableDropdown
+      //   key="actionGroup"
+      //   onSelect={() => action?.reload()}
+      //   menus={[
+      //     { key: 'copy', name: '复制' },
+      //     { key: 'delete', name: '删除' },
+      //   ]}
+      // />,
+    ],
   },
 ];
 
@@ -72,6 +108,7 @@ const getList = async (params: { page: number; size: number }) => {
 const Lesson: React.FC = () => {
   const [list, updateList] = useState<RowItem[]>([]);
   const [page, updatePage] = useState<number>(0);
+  const actionRef = useRef<ActionType>();
 
   useEffect(() => {
     request.getLessonList({ page, size: 10 }).then((res) => {
@@ -82,7 +119,12 @@ const Lesson: React.FC = () => {
 
   return (
     <PageContainer>
-      <ProTable<RowItem> columns={columns} dataSource={list}></ProTable>
+      <ProTable<RowItem>
+        rowKey={'lessonId'}
+        columns={columns}
+        dataSource={list}
+        actionRef={actionRef}
+      ></ProTable>
     </PageContainer>
   );
 };
